13d97448adbd512832c3438bbc0e2b23eb60eba4
Kapil <kapilraut026@gmail.com>
2025-09-28 22:43:00 +0545

    hello

---
+++ Day1.py
@@ -0,0 +1,29 @@
+# students = 9
+# rating = 4.99
+# print(students)
+import math
+
+box = "Python is the legend."
+# nessage = "it is nice one..."
+# meta = "it is the box i\"d so is it right..."
+
+# print(len(box))
+# print(box[0:8])
+# print(meta)
+
+# first = "King"
+# last = "Kapil"
+
+# full = f"{first} {last}"
+# print(full)
+
+print(box.upper())
+# print(box.find("hero"))
+
+x = 1
+x = 1.1
+print(round(x))
+print(math.ceil(2.2))
+a = input("x: ")
+y = int(a) + 15
+print(y)
